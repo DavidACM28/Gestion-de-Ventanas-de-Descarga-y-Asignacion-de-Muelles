7cd396c7e946e7c959e37bae9240054d0fdb8db2
DavidACM28 <dacasmore6@gmail.com>
2026-03-10 11:13:50 -0500

    feat(dto): add ErrorResponseDTO for API error responses

---
+++ src/main/java/pe/incubadora/backend/dtos/ErrorResponseDTO.java
@@ -0,0 +1,13 @@
+package pe.incubadora.backend.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponseDTO {
+    private String code;
+    private String message;
+}
